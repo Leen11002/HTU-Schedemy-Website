d76a9c81cc6e02bdbcd59985d31d0bae30b68c24
leenhija <leenhija171@gmail.com>
2026-01-19 17:22:30 +0300

    change backend ip

--- src/app/api/ta/route.ts
+++ src/app/api/ta/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-const TA_API_URL = 'http://54.145.35.49:8080/instructor/ta';
+const TA_API_URL = 'http://54.242.249.203:8080/instructor/ta';
 
 export async function GET() {
   try {
